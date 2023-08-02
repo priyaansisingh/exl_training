997ad16c8e6a44dc230a627b3cb8c65a6b75d1b8
Priyaansi Singh <54908765+priyaansisingh@users.noreply.github.com>
2023-08-02 12:22:08 +0530

    Add files via upload

---
+++ Program4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3
+{
+    
+    internal class Program4
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 34000, 24000, 31000, 28000, 29000, 33000 };
+
+            var result = numbers.Select(x => x * 1.03);
+
+            foreach (var i in result)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
